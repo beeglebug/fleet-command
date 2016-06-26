115c4dbea88b2246ce76786aff2f21b9b0e8e6af
Stuart Lee <stuart.lee@drpgroup.com>
2016-06-26 11:07:32 +0100

    add a test ship

--- src/index.js
+++ src/index.js
@@ -32,6 +32,35 @@ function makeOrbitalBody(bodySize, orbitalDistance) {
   };
 }
 
+function makeDot() {
+
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+
+  var material = new THREE.PointCloudMaterial( { size: 1, sizeAttenuation: false } );
+
+  var dot = new THREE.PointCloud( geometry, dotMaterial );
+  scene.add( dot );
+
+}
+
+function makeShip() {
+
+  var material = new THREE.LineBasicMaterial({ color: 0xffaa00 });
+  var geometry = new THREE.Geometry();
+
+  geometry.vertices = [
+    new THREE.Vector3(0, -2, 0),
+    new THREE.Vector3(1, 0, 0),
+    new THREE.Vector3(0, 2, 0),
+    new THREE.Vector3(-1, 0, 0),
+    new THREE.Vector3(0, -2, 0)
+  ];
+
+
+  return new THREE.Line(geometry, material);
+}
+
 function makeCircle(size, color, rotate) {
 
   if(rotate === undefined) { rotate = false; }
@@ -71,8 +100,11 @@ container.add(moon1.body);
 container.add(moon2.body);
 
 
+var ship = makeShip();
 
+container.add(ship);
 
+ship.position.x = 42;
 
 var axisHelper = new THREE.AxisHelper(10);
 scene.add( axisHelper );
@@ -98,6 +130,7 @@ function update(delta) {
   moon1.body.position.setFromMatrixPosition(moon1.root.matrixWorld);
   moon2.body.position.setFromMatrixPosition(moon2.root.matrixWorld);
 
+  ship.lookAt(camera.position);
   planet.body.lookAt(camera.position);
   moon1.body.lookAt(camera.position);
   moon2.body.lookAt(camera.position);
