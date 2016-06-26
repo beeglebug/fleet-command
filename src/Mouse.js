08e6c39ac43b53249ab2dcfc2307d9688be88f13
Stuart Lee <stuart.lee@drpgroup.com>
2016-06-26 18:42:42 +0100

    use 2d vector

--- src/Mouse.js
+++ src/Mouse.js
@@ -2,14 +2,13 @@ var THREE = require('three');
 
 var Mouse = {
 
-  position: new THREE.Vector3(),
+  position: new THREE.Vector2(),
 
-  update: function (event) {
+  update: function(event) {
 
     this.position.set(
       (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1,
-      0
+      -(event.clientY / window.innerHeight) * 2 + 1
     );
   }
 };
